f2d46b3d229aaa03e6e8c4dc8213d32e3c82ed4a
Ilya Strugatskiy <is368@cornell.edu>
2025-12-25 17:25:06 -0500

    remove useless logs

--- packages/riscv-compiler/src/compiler.ts
+++ packages/riscv-compiler/src/compiler.ts
@@ -7,8 +7,6 @@ export const compile_riscv = bytecode_of_string.bind(undefined, [
     ...pseudo_macros,
 ]);
 
-console.log(core_macros.toSorted(({ name }, b) => name.localeCompare(b.name)));
-console.log(pseudo_macros);
 export * from './ast/ast_utils';
 export { parser as riscv_parser } from './ast/riscv';
 export * from './arguments';
